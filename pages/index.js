2a2092f7ad56fb39efc5a6f85983aa078dfb3610
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-23 17:30:09 +0530

    banner comp Title SubT img retrieved

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Banner from '../components/banner/banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,13 +10,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        
+
         <title>Netflix</title>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
      
       <h1>Netflix</h1>
+
+          <Banner
+          title="Harry Potter"
+          subTitle="Mysterious Rise"
+          imgUrl="/static/HP.webp"
+          />
+         {/* 
+         <Nav Bar
+         <Card />
+      */}
     </div>
   )
 }
