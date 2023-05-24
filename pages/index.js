37706ae87dcc8146f03053080a1ed9f340279940
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-24 17:13:52 +0530

    nav component implemented

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Banner from '../components/banner/banner'
-
+import NavBar from '@/components/nav/navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,8 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
      
-      <h1>Netflix</h1>
-
+      
+          <NavBar username="TomRiddle@hogwartz.com"/>
           <Banner
           title="Harry Potter"
           subTitle="Mysterious Rise"
