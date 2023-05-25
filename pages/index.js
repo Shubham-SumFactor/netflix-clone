bafd02dc15ac82a68d8f0f013458c70c765f8f67
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-25 15:30:34 +0530

    cards added along with videos object

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from "next/image"
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Banner from '../components/banner/banner'
 import NavBar from '@/components/nav/navbar'
 import Card from '@/components/card/card'
+import SectionCards from "../components/card/section-cards"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const disneyVideos = [
+    {
+      imgUrl:"/static/HP.webp",
+    },
+    {
+      imgUrl:"/static/HP.webp",
+    },
+    {
+      imgUrl:"/static/HP.webp",
+    }
+  ]
   return (
     <div className={styles.container}>
       <Head>
@@ -25,14 +37,11 @@ export default function Home() {
           imgUrl="/static/HP.webp"
           />
          
-         <Card imgUrl='/static/HP.webp'
-          size="large" />
-         
-         <Card imgUrl='/static/HP.webp'
-          size="medium" />
+         <div className={styles.sectionWrapper}>
+          <SectionCards  title = "Disney" videos={disneyVideos} size="large"/>
+          <SectionCards  title = "Productivity" videos={disneyVideos} size="medium"/>
+          </div>
 
-         <Card imgUrl='/static/HP.webp'
-          size="small" />
     </div>
   )
 }
