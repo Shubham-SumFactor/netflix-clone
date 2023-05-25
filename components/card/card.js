0e154f3d7a2ff3e81e14c1cecf763e933fbabb78
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-25 15:29:01 +0530

    Section card for category added

--- components/card/card.js
+++ components/card/card.js
@@ -6,7 +6,10 @@ import cls from "classnames";
 
 const Card = (props) => {
 
-    const { imgUrl ="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=859&q=80", size = "medium" } = props;
+    const { imgUrl ="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=859&q=80",
+     size = "medium",
+    id 
+    } = props;
     const [imgSrc, setImgSrc] = useState(imgUrl);
     const classMap ={
         large: styles.lgItem,
@@ -17,13 +20,14 @@ const Card = (props) => {
     const handleOnError = () =>{
         console.log('hi error');
         setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=859&q=80");
-    }
+    };
 
+    const scale = id === 0 ? { scaleY:1.1 } : { scale:1.1 }
     return(
         <div className={styles.container} >
-        Im a card
+        
                 <motion.div 
-                className={cls(styles.imgMotionWrapper,classMap[size])} whileHover={{scale: 1.2}}>
+                className={cls(styles.imgMotionWrapper,classMap[size])} whileHover={{...scale}}>
                      <Image
                          src={ imgSrc }
                          alt="Img"
