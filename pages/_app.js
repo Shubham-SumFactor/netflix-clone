0028bb8f8c6bc6c76cac1e84c6f0b32b9a3f17c2
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-29 04:00:27 +0530

    Flickering while refreshing login page fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
 import '@/styles/globals.css'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react'
+import { magic } from '../lib/magic-client';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+ const [isLoading, setIsLoading] = useState(true);
+ 
+  useEffect(async ()=> {
+
+   const isLoggedIn = await magic.user.isLoggedIn();
+
+   if(isLoggedIn) {
+   // setIsLoading(false);
+    router.push('/');
+   }
+   else{
+    //setIsLoading(false);
+    router.push('/login')
+   }
+  },[]);
+
+  useEffect(() => {
+
+    const handleComplete = () => {
+        setIsLoading(false);
+    }
+
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+   
+
+    return () => {
+    router.events.off('routeChangeComplete', handleComplete);
+    router.events.off('routeChangeError', handleComplete);
+};
+}, [router]);
+
+  return isLoading ? <div>Loading....</div> : 
+
+
+   <Component {...pageProps} />
 }
