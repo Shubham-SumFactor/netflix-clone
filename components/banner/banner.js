2558aedcac71367ea3715bbfe128b6da38f65ef1
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-29 12:08:10 +0530

    play button routes to video/videoId

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,12 +1,15 @@
 import styles from './banner.module.css'
 import Image from 'next/image';
+import { useRouter } from 'next/router'; 
 
 const Banner = (props) =>{
 
-    const { title, subTitle, imgUrl } = props;
+    const { title, subTitle, imgUrl, videoId } = props;
+    const router = useRouter();
 
     const handleOnPlay = () => {
-        console.log('handleOnPlay')
+        console.log('handleOnPlay');
+        router.push(`video/${videoId}`);
     }
 
     return ( 
