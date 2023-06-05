5fa8f0c7675371c7f4fe54979f875dd5769b76f3
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-05 16:54:02 +0530

    Stat API JWT verification + query dtring for videoID

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,7 +1,34 @@
+import jwt from "jsonwebtoken";
+import { findVideoIdByUser } from "@/lib/db/hasura";
+
 export default async function stats(req, resp) {
     if(req.method === "POST"){ 
+       // console.log({ cookies: req.cookies });
 
-            resp.send({ msg: "it works" })
+        try {
+        const token = req.cookies.token
+        if(!token){
+
+            resp.status(403).send({});
+        }else{
+
+            const videoId = req.query.videoId;
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          //  console.log({ decodedToken });
+
+            const userId = decodedToken.issuer;
+          //  const videoId = '4zH5iYM4wJo';
+
+            const findVideoId = await findVideoIdByUser(token, userId, videoId);
+           // console.log({ findVideoId })
+            resp.send({ msg: "it works", decodedToken, findVideoId });
+
+        }
+            
+    }catch(error){
+        console.log("error occured /stats", error);
+        resp.status(500).send({done: false, error: error?.message })
     }
-
 }
+}
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweDA1Y2E1RTJDQjk0MmViNjRiRDkyQjg2M2NkMjBFQjU2MkI3OGNFOTEiLCJwdWJsaWNBZGRyZXNzIjoiMHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIiwiZW1haWwiOiJzaHViaGFtLnN1bWZhY3RvckBnbWFpbC5jb20iLCJvYXV0aFByb3ZpZGVyIjpudWxsLCJwaG9uZU51bWJlciI6bnVsbCwid2FsbGV0cyI6W10sImlhdCI6MTY4NTY5MDk4NiwiZXhwIjoxNjg2Mjk1Nzg2LCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS11c2VyLWlkIjoiZGlkOmV0aHI6MHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIn19.mVcbZF7pNfEz6Txru0Kpu1H5CD63143hKIuGANtsEfE
