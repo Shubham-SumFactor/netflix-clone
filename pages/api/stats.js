081909ad26d2b9b23c49802ab97a8ff528f5f67f
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-07 11:18:21 +0530

    Tweak stats API GET Request

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,56 +1,60 @@
 import jwt from "jsonwebtoken";
-import { findVideoIdByUser, updateStats, insertStats } from "@/lib/db/hasura";
+import { findVideoIdByUser, updateStats, insertStats } from '@/lib/db/hasura';
+
 
 export default async function stats(req, resp) {
-    if(req.method === "POST"){ 
-       // console.log({ cookies: req.cookies });
 
-        try {
-        const token = req.cookies.token
-        if(!token){
-
+    try {
+        const token = req.cookies.token;
+        if (!token) {
             resp.status(403).send({});
-        }else{
-            const { videoId, favourited, watched = true} = req.body;
-            if (videoId){
+        } else {
+            const inputParams = req.method === 'POST' ? req.body : req.query;
+            const { videoId } = inputParams;
+
+            if (videoId) {
                 const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
                 const userId = decodedToken.issuer;
+                const findVideo = await findVideoIdByUser(
+                    token,
+                    userId,
+                    videoId
+                );
 
-                const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
-                 
-                 if (doesStatsExist) {
-                  // update it 
-                  const response = await updateStats(token, {
-                      
-                      watched,
-                      userId,
-                      videoId,
-                      favourited,
-                  });
-                  resp.send({ data: response });
-      
-                 }else{
-                  //add it 
-                  const response = await insertStats(token, {
-                      
-                      watched,
-                      userId,
-                      videoId,
-                      favourited,
-                  });
-                  resp.send({ data: response });
-      
-                 }
-                 
-              }
+                const doesStatsExits = findVideo?.length > 0;
+                if (req.method === "POST") {
+                    const { favourited, watched = true } = req.body;
+                    if (doesStatsExits) {
+                        const response = await updateStats(token, {
+                            favourited,
+                            watched,
+                            videoId,
+                            userId,
+                        });
+                        resp.send({ data: response });
+                    } else {
+                        const response = await insertStats(token, {
+                            favourited,
+                            watched,
+                            videoId,
+                            userId,
+                        });
+                        resp.send({ data: response });
+                    }
 
+                } else {
+                    if (doesStatsExits) {
+                        resp.send(findVideo);
+                    } else {
+                        resp.status(404);
+                        resp.send({ user: null, msg: "video not found" });
+                    }
+                }
             }
-            
-    }catch(error){
-        console.log("error occured /stats", error);
-        resp.status(500).send({done: false, error: error?.message })
+        }
+    } catch (error) {
+        console.error("Error occured /stats", error);
+        resp.status(500).send({ done: false, error: error?.message });
     }
-}
 }
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweDA1Y2E1RTJDQjk0MmViNjRiRDkyQjg2M2NkMjBFQjU2MkI3OGNFOTEiLCJwdWJsaWNBZGRyZXNzIjoiMHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIiwiZW1haWwiOiJzaHViaGFtLnN1bWZhY3RvckBnbWFpbC5jb20iLCJvYXV0aFByb3ZpZGVyIjpudWxsLCJwaG9uZU51bWJlciI6bnVsbCwid2FsbGV0cyI6W10sImlhdCI6MTY4NTY5MDk4NiwiZXhwIjoxNjg2Mjk1Nzg2LCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS11c2VyLWlkIjoiZGlkOmV0aHI6MHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIn19.mVcbZF7pNfEz6Txru0Kpu1H5CD63143hKIuGANtsEfE
