2d91d7f75dd0d0656a943e173ec470cfa76a6765
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-08 00:30:35 +0530

    imported redirectuser hook

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,13 @@ import SectionCards from "../components/card/section-cards"
 const inter = Inter({ subsets: ['latin'] })
 import { getPopularVideos, getVideos, getWatchItAgainVideos } from '@/lib/videos'
 
+import useRedirectUser from '@/utils/redirectUser'
 
+export async function getServerSideProps(context) {
+  const { userId, token } = await useRedirectUser(context);
 
-export async function getServerSideProps() {
-
-  const userId = "did:ethr:0x05ca5E2CB942eb64bD92B863cd20EB562B78cE91";
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweDA1Y2E1RTJDQjk0MmViNjRiRDkyQjg2M2NkMjBFQjU2MkI3OGNFOTEiLCJwdWJsaWNBZGRyZXNzIjoiMHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIiwiZW1haWwiOiJzaHViaGFtLnN1bWZhY3RvckBnbWFpbC5jb20iLCJvYXV0aFByb3ZpZGVyIjpudWxsLCJwaG9uZU51bWJlciI6bnVsbCwid2FsbGV0cyI6W10sImlhdCI6MTY4NTcwOTM2NCwiZXhwIjoxNjg2MzE0MTY0LCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS11c2VyLWlkIjoiZGlkOmV0aHI6MHgwNWNhNUUyQ0I5NDJlYjY0YkQ5MkI4NjNjZDIwRUI1NjJCNzhjRTkxIn19.OnYxfhsN_vmwLus9cCMGusVVePBYYssr4xbWAlquvMc";
   const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
-  console.log( { watchItAgainVideos } );
+
   const disneyVideos =  await getVideos("disney trailer");
   const productivityVideos =  await getVideos("productivity trailer");
   const travelVideos =  await getVideos("travel trailer");
@@ -27,7 +26,7 @@ export async function getServerSideProps() {
     { disneyVideos, productivityVideos, travelVideos, popularVideos, watchItAgainVideos } };
 }
 
-export default function Home( { disneyVideos, productivityVideos, travelVideos, popularVideos, watchItAgainVideos } ) {
+export default function Home( { disneyVideos, productivityVideos, travelVideos, popularVideos, watchItAgainVideos = [] } ) {
 
 
   return (
