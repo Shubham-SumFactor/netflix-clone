88ef426c5a9426a2e2ea29de0ea7f2eb808b0b18
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-08 20:36:31 +0530

    props for mylist vids added

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -1,4 +1,37 @@
-const MyList = () => {
-    return <div> MyList </div>
+import SectionCards from "@/components/card/section-cards";
+import Navbar from "@/components/nav/navbar";
+import Head from "next/head";
+import styles from "../../styles/MyList.module.css"
+import { getMyList } from "@/lib/videos";
+import useRedirectUser from "@/utils/redirectUser";
+
+export async function getServerSideProps(context) {
+  const { userId, token } = await useRedirectUser(context);
+
+    const  videos = await getMyList(userId, token);
+
+    return { props: 
+        {  
+           myListVideos: videos ,
+        } 
+        };
+    }
+const MyList = ({ myListVideos }) => {
+    return ( <div> 
+                <Head> 
+                    <title> My list</title>
+                </Head>
+
+                <main className={styles.main}>
+                    <Navbar />
+                    <div className={styles.sectionWrapper}>
+                        <SectionCards  
+                        title = "My List" 
+                        videos={myListVideos} 
+                        size="small"/>
+                    </div>
+                </main>
+            </div>
+);
 };
 export default MyList;
