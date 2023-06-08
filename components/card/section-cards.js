5cdd13ca5708bc556819c8423bdaed44df3e4ba9
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-09 00:18:55 +0530

    should scale added to props

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,20 +1,22 @@
 import Card from "./card";
 import styles from "./section-cards.module.css"
 import Link from "next/link";
+import clsx from "classnames";
 
 const SectionCards = (props) => {
 
-    const { title, videos = [], size } = props;
+    const { title, videos = [], size, shouldWrap = false, shouldScale} = props;
        // console.log({ videos });
     return (
         <section className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
-            <div className={styles.cardWrapper}>
+            <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
                 { videos.map((video, idx) => {
                     return <Link key={idx} href={`/video/${video.id}`}>
                         
                     <Card key={idx} id = {idx} imgUrl={video.imgUrl}
-                size={size} />
+                size={size} 
+                shouldScale = {shouldScale}/>
                 </Link>
                 })}
             </div>
