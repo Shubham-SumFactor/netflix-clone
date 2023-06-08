c9e3e9f1081d829d9ffbb42f289a1f9b2cbba259
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-08 14:10:26 +0530

    router push updated

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
     };
     const handleOnClickMyList = (e) => {
         e.preventDefault();
-        router.push('/browse/my-List');
+        router.push('/browse/my-list');
     };
 
     const handleShowDropdown =(e) =>{
